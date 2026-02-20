9208ec66cdff2e530179d92004faa2d7f0cec066
Eduardo Tiago <eduardotiago@hotmail.com>
2026-02-19 21:16:38 -0300

    Revert "Alterado de 100 para 1000 o total de numeros"
    This reverts commit 26d15973a27c4d9f92999544d26ed75c881840fc.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
+//novo teste
 let listaDeNumerosSorteados = [];
-let valorMaximo = 1000;
+let valorMaximo = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 exibirTextoNaTela("h1", "Jogo do número secreto!");
